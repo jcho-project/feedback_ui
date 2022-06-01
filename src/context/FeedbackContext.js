a7406912c7e10959a0fefad0803f22ce8c9ca45d
Junehyok Cho <jcho0627@gmail.com>
2022-06-01 17:46:36 +0200

    update and delete from json-server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,8 +22,10 @@ export const FeedbackProvider = ({children}) => {
     }
 
     // Delete feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if(window.confirm("Are you sure you want to delete?")) {
+            await fetch(`/feedback/${id}`, {method: "DELETE"})
+
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
@@ -52,8 +54,18 @@ export const FeedbackProvider = ({children}) => {
     }
 
     // Update feedback item
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem} : item))
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updItem),
+        })
+
+        const data = await response.json()
+
+        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data} : item))
     }
 
     return <FeedbackContext.Provider value={{
